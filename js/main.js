467786a8b9f39290966a5efc65315767e9f03b4f
l77e <lukeedgar@outlook.com>
2018-11-19 10:38:37 +0000

    Additional content

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let currentArticle1 = {
     "text": "",
     "source": "",
     "isMisleading": null,
-    "reason" : ""
+    "reason": ""
 }
 
 let currentArticle2 = {
@@ -17,28 +17,56 @@ let currentArticle2 = {
 }
 
 const articlesJSON = [{
-    "title": "Stephen Barclay named new Brexit Secretary",
-    "text": "Stephen Barclay has been confirmed as the new Brexit Secretary as Theresa May seeks to fill posts in her cabinet.",
-    "source": "https://www.bbc.co.uk/news/uk-46241693",
-    "isMisleading": false,
-    "reason" : "BBC is a trusted news source"
-},
-{
-"title": "Ada is organised",
-    "text": "Ada staff know what they are doing",
-    "source": "https://www.bbc.co.uk/news/uk-46241693",
-    "isMisleading": true,
-    "reason": "Ada staff are not organised"
-}]
+        "title": "Stephen Barclay named new Brexit Secretary",
+        "text": "Stephen Barclay has been confirmed as the new Brexit Secretary as Theresa May seeks to fill posts in her cabinet.",
+        "source": "https://www.bbc.co.uk/news/uk-46241693",
+        "isMisleading": false,
+        "reason": "BBC is a trusted news source"
+    },
+    {
+        "title": "Ada is organised",
+        "text": "Ada staff know what they are doing",
+        "source": "https://www.bbc.co.uk/news/uk-46241693",
+        "isMisleading": true,
+        "reason": "Ada staff are not organised"
+    }, {
+        "title": "You'll never believe how much weight this celebrity lost in a month!",
+        "text": "",
+        "source": "www.express.co.uk",
+        "isMisleading": true,
+        "reason": "This is not fake news (clickbait) because… the context is upfront and apparent and we have the name of a person so we can verify the information by searching. We have no context to who the celebrity is, and it is using persuasive language to lure in the reader instead of facts."
+    },
+    {
+        "title": "Queen Elizabeth Removes Obamas For Royal Wedding Guest List",
+        "text": `For eight years, Michelle Obama genuinely thought she was a queen and expected to be treated like one. Now that she’s a regular citizen, she’s having a hard time of letting go of her former status as the (failed) first lady and keeps trying to push her way into every major social scene where she doesn’t belong and is apparently not invited.
+When she heard the news that Prince Harry was marrying a black American girl, she assumed it was her place to be on that guest list, which has to be approved by the Queen of England herself. Michelle made a desperation move to sneak a spot on that exclusive guest list and as soon as the real queen found out what the wannabe royalty did, she delivered a brutal beatdown that was definitely deserved.
+As soon as the big news about the royal engagement of Prince Harry and an American actress Meghan Markle was announced, the Obamas saw an opportunity to edge their way into the elites, like a couple of desperate kids trying to hang with the popular crowd at school. Barack immediately got on social media to beat President Trump to the congratulatory punch. He has a lot more time on his hands than the Commander-in-Chief who is busy cleaning up his mess.
+Barack practically stalked the prince on Twitter and made a post within seconds of the news coming out, which perhaps had an undertone of letting the royals know that he and Michelle were available that day to attend.`,
+        "source": "https://archive.fo/1cJLE#selection-641.0-659.217",
+        "isMisleading": true,
+        "reason": "This is fake news (a false story) because…it is worded in a way that comes across as there is less background of their story and is less likely to be true."
+    },
+    {
+        "title": "Queen Elizabeth Has Reportedly Invited Meghan Markle's Mother Doria to Spend Christmas With the Royals",
+        "text": `Meghan Markle's mother Doria Ragland made a big splash at the royal wedding. Despite all of the pre-wedding drama caused by Meghan Markle's father, Doria was poised, collected, and there for Meghan (as the mother of the bride should be).
+
+In the days leading up to the wedding, Ragland made time to have tea with the Queen, where the two supposedly discussed—with Meghan and Prince Harry—whether or not Meghan Markle's father Thomas Markle should still attend the wedding (after he staged paparazzi photos for money and then spread lies to gain public sympathy). 
+It's clear that Doria earned the Queen's trust during this time, as the The Daily Mail is reporting that Queen Elizabeth has invited Meghan Markle's mother to spend Christmas with the royals this year, an extremely rare offer that hasn't ever been extended to Kate Middleton's family.`,
+        "source": "https://www.elle.com/culture/celebrities/a24629749/queen-elizabeth-meghan-markle-mother-doria-christmas-invitation/",
+        "isMisleading": false,
+        "reason": "This is not fake news (a false story) because…it states ‘reportedly’ so this means that the people presenting think that the source is not 100% reliable but this is still real news."
+    }
+]
+
 
 function getRandomArticle(isMisleading) {
     //Fiter articles depending on is isMisleading param
-    let filteredArticles =  articlesJSON.filter(article => {
+    let filteredArticles = articlesJSON.filter(article => {
         return article.isMisleading == isMisleading
     })
 
     //Return a random article from the filtered array filteredArticles
-    return filteredArticles[0, filteredArticles.length - 1]
+    return filteredArticles[randomNumber(0, filteredArticles.length)]
 }
 
 //Return a random Number between two values
@@ -90,15 +118,15 @@ function getArticleSet() {
     updateArticle(2, misleadingArticle);
 }
 
-function leftArticle(){
+function leftArticle() {
     showReason();
 }
 
-function rightArticle(){
+function rightArticle() {
     showReason();
 }
 
-function showReason(){
+function showReason() {
     //document.body.style.background = 'red';
 
 
@@ -110,15 +138,20 @@ function showReason(){
 
     document.getElementById('reasonTitle').innerHTML = `Reasons`
 
-    document.getElementById('reasonText').innerHTML = `<ul><li>${reason1}</li><li>${reason2}</li></ul>`
-    
+    document.getElementById('reasonText').innerHTML =
+        `<ul>
+    <h3>Left Article</h3>
+    <li>${reason1}</li>
+    <h3>Right Article</h3>
+    <li>${reason2}</li></ul>`
+
 }
 
-function hideReason(){
+function hideReason() {
     document.getElementById('reason-container').style.display = "none";
 }
 
-function nextQuestion(){
+function nextQuestion() {
     hideReason()
     getArticleSet()
 }
@@ -129,5 +162,5 @@ getArticleSet();
 let article1Btn = document.getElementById('article1')
 let article2Btn = document.getElementById('article2')
 
-article1Btn.addEventListener("click",leftArticle, false)
-article2Btn.addEventListener("click",rightArticle, false)
+article1Btn.addEventListener("click", leftArticle, false)
+article2Btn.addEventListener("click", rightArticle, false)
